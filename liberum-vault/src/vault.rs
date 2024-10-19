43b4859374f69004defea1b92ef7d3df665d7989
Maciej Krzyżanowski <krzmaciek@gmail.com>
2024-10-19 21:30:59 +0200

    liberum-vault: Rewrite using kameo

--- liberum-vault/src/vault.rs
+++ liberum-vault/src/vault.rs
@@ -1,156 +1,161 @@
+use std::path::Path;
+
 use anyhow::anyhow;
 use anyhow::Result;
+use kameo::mailbox::bounded::BoundedMailbox;
+use kameo::message::{Context, Message};
+use kameo::Actor;
 use rusqlite::OptionalExtension;
-use tokio::task::JoinHandle;
 use tokio_rusqlite::Connection;
-use tokio::sync::{oneshot, mpsc};
 
 use crate::fragment::key::Key;
 use crate::fragment::Fragment;
 
-type StoreResult = Result<()>;
-type GetResult = Result<Option<Fragment>>;
-type RemoveResult = Result<()>;
-
-type StoreResponder = tokio::sync::oneshot::Sender<StoreResult>;
-type GetResponder = tokio::sync::oneshot::Sender<GetResult>;
-type RemoveResponder = tokio::sync::oneshot::Sender<RemoveResult>;
-
-pub enum Query {
-    Store {
-        fragment: Fragment,
-        resp: StoreResponder,
-    },
-    Get {
-        key: Key,
-        resp: GetResponder,
-    },
-    Remove {
-        key: Key,
-        resp: RemoveResponder,
-    },
+pub struct Vault {
+    db: Connection,
 }
 
-pub async fn prepare_db(db: &Connection) -> Result<()> {
-    const CREATE_FRAGMENT_TABLE_QUERY: &str = "
-        CREATE TABLE IF NOT EXISTS fragment (
-            hash0 INTEGER,
-            hash1 INTEGER,
-            hash2 INTEGER,
-            hash3 INTEGER,
-            path VARCHAR(255),
-            size INTEGER,
-            PRIMARY KEY (hash0, hash1, hash2, hash3)
-        )
-    ";
+pub struct Store(pub Fragment);
+pub struct Get(pub Key);
+pub struct Remove(pub Key);
 
-    db.call(|conn| {
-        conn.execute(CREATE_FRAGMENT_TABLE_QUERY, ())?;
+impl Vault {
+    pub async fn new(db_path: &Path) -> Result<Vault> {
+        let db = Connection::open(db_path).await?;
 
-        Ok(())
-    })
-    .await?;
+        Ok(Vault { db })
+    }
 
-    Ok(())
-}
+    pub async fn in_memory() -> Result<Vault> {
+        let db = Connection::open_in_memory().await?;
 
-pub async fn handle_queries(db: Connection, mut queries: mpsc::Receiver<Query>) -> JoinHandle<()> {
-    tokio::spawn(async move {
-        loop {
-            if let Some(query) = queries.recv().await {
-                handle_query(&db, query).await;
-            } else {
-                break;
-            }
-        }
-    })
-}
-
-async fn handle_query(db: &Connection, query: Query) {
-    match query {
-        Query::Store { fragment, resp } => handle_store(db, fragment, resp).await,
-        Query::Get { key, resp } => handle_get(db, key, resp).await,
-        Query::Remove { key, resp } => handle_remove(db, key, resp).await,
+        Ok(Vault { db })
     }
 }
 
-async fn handle_store(db: &Connection, fragment: Fragment, resp: StoreResponder) {
-    const INSERT_FRAGMENT_QUERY: &str = "
-        INSERT INTO fragment (hash0, hash1, hash2, hash3, path, size)
-        VALUES (?1, ?2, ?3, ?4, ?5, ?6)
-    ";
-    let hash_as_u64 = fragment.hash.as_u64_slice_be();
+impl Actor for Vault {
+    type Mailbox = BoundedMailbox<Self>;
 
-    let result = db
-        .call(move |conn| {
-            conn.execute(
-                INSERT_FRAGMENT_QUERY,
-                (
-                    hash_as_u64[0] as i64,
-                    hash_as_u64[1] as i64,
-                    hash_as_u64[2] as i64,
-                    hash_as_u64[3] as i64,
-                    fragment.path,
-                    fragment.size,
-                ),
-            )?;
+    async fn on_start(
+        &mut self,
+        _: kameo::actor::ActorRef<Self>,
+    ) -> std::result::Result<(), kameo::error::BoxError> {
+        const CREATE_FRAGMENT_TABLE_QUERY: &str = "
+            CREATE TABLE IF NOT EXISTS fragment (
+                hash0 INTEGER,
+                hash1 INTEGER,
+                hash2 INTEGER,
+                hash3 INTEGER,
+                path VARCHAR(255),
+                size INTEGER,
+                PRIMARY KEY (hash0, hash1, hash2, hash3)
+            )
+        ";
 
-            Ok(())
-        })
-        .await;
+        self.db
+            .call(|conn| Ok(conn.execute(CREATE_FRAGMENT_TABLE_QUERY, ())?))
+            .await?;
 
-    let _ = resp.send(result.map_err(|e| anyhow!(e)));
+        Ok(())
+    }
 }
 
-async fn handle_get(db: &Connection, key: Key, resp: GetResponder) {
-    const SELECT_FRAGMENT_QUERY: &str = "
-        SELECT path, size
-        FROM fragment
-        WHERE hash0 = ?1 AND hash1 = ?2 AND hash2 = ?3 AND hash3 = ?4
-    ";
+impl Message<Store> for Vault {
+    type Reply = Result<()>;
 
-    let result = db
-        .call(move |conn| {
-            let mut stmt = conn.prepare(SELECT_FRAGMENT_QUERY)?;
-            let key_u64_slice = key.as_u64_slice_be();
-            let key_as_i64 = [
-                key_u64_slice[0] as i64,
-                key_u64_slice[1] as i64,
-                key_u64_slice[2] as i64,
-                key_u64_slice[3] as i64,
-            ];
+    async fn handle(
+        &mut self,
+        Store(fragment): Store,
+        _: Context<'_, Self, Self::Reply>,
+    ) -> Self::Reply {
+        const INSERT_FRAGMENT_QUERY: &str = "
+                INSERT INTO fragment (hash0, hash1, hash2, hash3, path, size)
+                VALUES (?1, ?2, ?3, ?4, ?5, ?6)
+            ";
+        let hash_as_u64 = fragment.hash.as_u64_slice_be();
 
-            let fragment = stmt
-                .query_row(key_as_i64, |r| {
-                    let path: String = match r.get(0) {
-                        Ok(p) => p,
-                        Err(e) => return Result::Err(e),
-                    };
+        self.db
+            .call(move |conn| {
+                conn.execute(
+                    INSERT_FRAGMENT_QUERY,
+                    (
+                        hash_as_u64[0] as i64,
+                        hash_as_u64[1] as i64,
+                        hash_as_u64[2] as i64,
+                        hash_as_u64[3] as i64,
+                        fragment.path,
+                        fragment.size,
+                    ),
+                )?;
 
-                    let size: u64 = match r.get(1) {
-                        Ok(s) => s,
-                        Err(e) => return Result::Err(e),
-                    };
-
-                    Result::Ok(Fragment::new(key, path, size))
-                })
-                .optional()?;
-
-            Ok(fragment)
-        })
-        .await;
-
-    let _ = resp.send(result.map_err(|e| anyhow!(e)));
+                Ok(())
+            })
+            .await
+            .map_err(|e| anyhow!(e))
+    }
 }
 
-async fn handle_remove(_: &Connection, _: Key, _: RemoveResponder) {
-    todo!()
+impl Message<Get> for Vault {
+    type Reply = Result<Option<Fragment>>;
+
+    async fn handle(&mut self, Get(key): Get, _: Context<'_, Self, Self::Reply>) -> Self::Reply {
+        const SELECT_FRAGMENT_QUERY: &str = "
+            SELECT path, size
+            FROM fragment
+            WHERE hash0 = ?1 AND hash1 = ?2 AND hash2 = ?3 AND hash3 = ?4
+        ";
+
+        self.db
+            .call(move |conn| {
+                let mut stmt = conn.prepare(SELECT_FRAGMENT_QUERY)?;
+                let key_u64_slice = key.as_u64_slice_be();
+                let key_as_i64 = [
+                    key_u64_slice[0] as i64,
+                    key_u64_slice[1] as i64,
+                    key_u64_slice[2] as i64,
+                    key_u64_slice[3] as i64,
+                ];
+
+                let fragment = stmt
+                    .query_row(key_as_i64, |r| {
+                        let path: String = match r.get(0) {
+                            Ok(p) => p,
+                            Err(e) => return Result::Err(e),
+                        };
+
+                        let size: u64 = match r.get(1) {
+                            Ok(s) => s,
+                            Err(e) => return Result::Err(e),
+                        };
+
+                        Result::Ok(Fragment::new(key, path, size))
+                    })
+                    .optional()?;
+
+                Ok(fragment)
+            })
+            .await
+            .map_err(|e| anyhow!(e))
+    }
 }
 
-pub async fn store_fragment(queries: &mpsc::Sender<Query>, fragment: Fragment) -> Result<()> {
-    let (tx, rx) = oneshot::channel();
+impl Message<Remove> for Vault {
+    type Reply = Result<()>;
 
-    queries.send(Query::Store { fragment, resp: tx }).await.map_err(|e| anyhow!(e))?;
+    async fn handle(&mut self, _: Remove, _: Context<'_, Self, Self::Reply>) -> Self::Reply {
+        todo!()
+    }
+}
 
-    rx.await.map_err(|e| anyhow!(e))?
+#[cfg(test)]
+mod tests {
+    use kameo::request::MessageSend;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn basic_test() {
+        let vault_ref = kameo::spawn(Vault::in_memory().await.unwrap());
+        vault_ref.ask(Store(Fragment::random())).send().await.unwrap();
+    }
 }
