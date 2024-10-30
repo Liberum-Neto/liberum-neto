d79ad4886503618c2a85579707a48788217f33fe
Maciej Krzyżanowski <krzmaciek@gmail.com>
2024-10-31 00:55:12 +0100

    node: Finish initial implementation

--- liberum_core/src/node.rs
+++ liberum_core/src/node.rs
@@ -1,41 +1,105 @@
-use std::path::{Path, PathBuf};
+use std::path::{self, Path, PathBuf};
 
-use anyhow::Result;
+use anyhow::{anyhow, bail, Result};
 use kameo::{message::Message, Actor};
-use libp2p::identity::Keypair;
-use serde::{Deserialize, Serialize};
+use libp2p::{identity::Keypair, Multiaddr, PeerId};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use thiserror::Error;
 
-const NODE_DIRECTORY_NAME: &str = ".liberum-neto";
 
 #[derive(Debug)]
 pub struct Node {
-    name: String,
-    keypair: Keypair,
+    pub name: String,
+    pub keypair: Keypair,
+    pub bootstrap_nodes: Vec<BootstrapNode>,
 }
 
 impl Node {
-    pub fn new(name: &str, keypair: Keypair) -> Self {
-        Self {
-            name: name.to_string(),
-            keypair,
+    const CONFIG_FILE_NAME: &'static str = "config.json";
+    const KEY_FILE_NAME: &'static str = "keypair";
+
+    pub fn builder() -> NodeBuilder {
+        NodeBuilder::default()
+    }
+
+    async fn load(node_dir_path: &Path) -> Result<Node> {
+        if !node_dir_path.is_dir() {
+            bail!("node_dir_path is not a directory");
         }
+
+        let config_path = node_dir_path.join(Node::CONFIG_FILE_NAME);
+        let config_bytes = tokio::fs::read(config_path).await?;
+        let config: NodeConfig = serde_json::from_slice(&config_bytes)?;
+        let key_path = node_dir_path.join(Node::KEY_FILE_NAME);
+        let key_bytes = tokio::fs::read(key_path).await?;
+        let keypair = Keypair::from_protobuf_encoding(&key_bytes)?;
+        let node_name = node_dir_path
+            .file_name()
+            .ok_or(anyhow!("incorrect node dir path, it should not end with .."))?
+            .to_str()
+            .ok_or(anyhow!("node dir path is not valid utf-8 string"))?
+            .to_string();
+        let node = Node::builder()
+            .name(node_name)
+            .config(config)
+            .keypair(keypair)
+            .build()?;
+
+        Ok(node)
     }
 
-    fn load(node_dir_path: &Path) -> Result<Node> {
-        todo!()
-    }
+    async fn save(&self, node_dir_path: &Path) -> Result<()> {
+        if !node_dir_path.is_dir() {
+            bail!("node_dir_path is not a directory");
+        }
 
-    fn save(&self, node_dir_path: &Path) -> Result<()> {
+        let config: NodeConfig = self.into();
+        let config_path = node_dir_path.join(Node::CONFIG_FILE_NAME);
+        let key_bytes = self.keypair.to_protobuf_encoding()?;
+        let key_path = node_dir_path.join(Node::KEY_FILE_NAME);
+
+        tokio::fs::write(key_path, key_bytes).await?;
+        tokio::fs::write(config_path, serde_json::to_string(&config)?).await?;
+
         todo!()
     }
 }
 
-impl TryFrom<NodeSerializable> for Node {
-    type Error = anyhow::Error;
+impl Into<NodeConfig> for &Node {
+    fn into(self) -> NodeConfig {
+        NodeConfig::new(self.bootstrap_nodes.clone())
+    }
+}
 
-    fn try_from(value: NodeSerializable) -> Result<Node> {
-        todo!()
+#[derive(Default)]
+pub struct NodeBuilder {
+    name: Option<String>,
+    keypair: Option<Keypair>,
+    bootstrap_nodes: Vec<BootstrapNode>,
+}
+
+impl NodeBuilder {
+    pub fn name(mut self, name: String) -> Self {
+        self.name = Some(name);
+        self
+    }
+
+    pub fn keypair(mut self, keypair: Keypair) -> Self {
+        self.keypair = Some(keypair);
+        self
+    }
+
+    pub fn config(mut self, config: NodeConfig) -> Self {
+        self.bootstrap_nodes = config.bootstrap_nodes;
+        self
+    }
+
+    pub fn build(self) -> Result<Node> {
+        return Ok(Node {
+            name: self.name.ok_or(anyhow!("node name is required"))?, 
+            keypair: self.keypair.ok_or(anyhow!("keypair is required"))?,
+            bootstrap_nodes: self.bootstrap_nodes, 
+        });
     }
 }
 
@@ -61,48 +125,83 @@ pub struct NodeStore {
 }
 
 impl NodeStore {
-    pub fn new() -> Result<Self> {
-        todo!()
+    const DEFAULT_NODES_DIRECTORY_NAME: &'static str = ".liberum-neto";
+
+    pub async fn new(nodes_dir_path: &Path) -> Result<Self> {
+        NodeStore::ensure_nodes_dir_path(nodes_dir_path).await?;
+        Ok(NodeStore { 
+            nodes_dir_path: nodes_dir_path.to_path_buf(),
+        })
     }
 
-    pub fn with_custom_nodes_dir(custom_nodes_dir_path: &Path) -> Result<Self> {
-        todo!()
+    pub async fn with_default_nodes_dir() -> Result<Self> {
+        let nodes_dir_path = NodeStore::resolve_nodes_dir_path(None);
+        NodeStore::new(nodes_dir_path).await
     }
 
-    fn load_node(&self, name: &str) -> Result<&Node> {
-        todo!()
+    pub async fn with_custom_nodes_dir(path: &Path) -> Result<Self> {
+        let nodes_dir_path = NodeStore::resolve_nodes_dir_path(Some(path));
+        NodeStore::new(nodes_dir_path).await
     }
 
-    fn save_node(&self, name: &str) -> Result<()> {
-        todo!()
+    async fn load_node(&self, name: &str) -> Result<Node> {
+        let node_dir_path = self.resolve_node_dir_path(name);
+        Node::load(&node_dir_path).await.map_err(|e| anyhow!("{}", e))
     }
 
-    fn node_dir_exists(&self, name: &str) -> Result<bool> {
-        todo!()
+    async fn save_node(&self, node: &Node) -> Result<()> {
+        let node_dir_path = self.resolve_node_dir_path(&node.name);
+        Node::save(node, &node_dir_path).await.map_err(|e| anyhow!("{}", e))
     }
 
-    fn resolve_node_dir_path(&self, name: &str) -> &Path {
-        todo!()
+    fn node_dir_exists(&self, name: &str) -> bool {
+        let node_dir_path = self.resolve_node_dir_path(name);
+        node_dir_path.exists()
     }
 
-    fn create_nodes_dir_path(&self) -> Result<()> {
-        todo!()
+    async fn ensure_node_dir_path(&self, name: &str) -> Result<()> {
+        if !self.node_dir_exists(name) {
+            let node_dir_path = self.resolve_node_dir_path(name);
+            tokio::fs::create_dir(node_dir_path).await?;
+        }
+
+        Ok(())
     }
 
-    fn resolve_nodes_dir_path(path_override: Option<&Path>) -> Result<&Path> {
-        todo!()
+    fn resolve_node_dir_path(&self, name: &str) -> PathBuf {
+       self.nodes_dir_path.join(name)
+    }
+
+    async fn ensure_nodes_dir_path(path: &Path) -> Result<()> {
+        tokio::fs::create_dir_all(path).await?;
+        Ok(())
+    }
+
+    fn resolve_nodes_dir_path(path_override: Option<&Path>) -> &Path {
+        if let Some(path_override) = path_override {
+            return path_override;
+        } else {
+            return Path::new(NodeStore::DEFAULT_NODES_DIRECTORY_NAME);
+        }
     }
 }
 
 impl Message<LoadNodes> for NodeStore {
-    type Reply = Result<Option<Node>, NodeStoreError>;
+    type Reply = Result<Vec<Node>, NodeStoreError>;
 
     async fn handle(
             &mut self,
             LoadNodes(names): LoadNodes,
             _: kameo::message::Context<'_, Self, Self::Reply>,
         ) -> Self::Reply {
-            Err(NodeStoreError::LoadError { name: "TODO".to_string() })
+            let mut result: Vec<Node> = Vec::new();
+            
+            for name in names {
+                let node = self.load_node(&name).await.map_err(|_| NodeStoreError::LoadError { name })?;
+                result.push(node);
+            }
+
+            Ok(result)
     }
 }
 
@@ -114,20 +213,56 @@ impl Message<StoreNodes> for NodeStore {
             StoreNodes(nodes): StoreNodes,
             _: kameo::message::Context<'_, Self, Self::Reply>,
         ) -> Self::Reply {
-            Err(NodeStoreError::StoreError { name: "TODO".to_string() })
+            for node in nodes {
+                self.save_node(&node).await.map_err(|_| NodeStoreError::StoreError { name: node.name })?;
+            }
+
+            Ok(())
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct BootstrapNode {
+    #[serde(serialize_with = "serialize_peer_id", deserialize_with = "deserialize_peer_id")]
+    id: PeerId,
+    addr: Multiaddr,
+}
+
+impl BootstrapNode {
+    pub fn new(peer_id: PeerId, addr: Multiaddr) -> Self {
+        BootstrapNode{
+            id: peer_id,
+            addr,
+        }
     }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct NodeSerializable {
-    name: String,
-    keypair: Vec<u8>,
+pub struct NodeConfig {
+    bootstrap_nodes: Vec<BootstrapNode>,
 }
 
-impl TryFrom<Node> for NodeSerializable {
-    type Error = anyhow::Error;
-
-    fn try_from(value: Node) -> Result<NodeSerializable> {
-        todo!()
+impl NodeConfig {
+    pub fn new(bootstrap_nodes: Vec<BootstrapNode>) -> Self {
+        Self {
+            bootstrap_nodes,
+        }
     }
+}
+
+fn serialize_peer_id<S>(peer_id: &PeerId, serializer: S) -> Result<S::Ok, S::Error> 
+where
+    S: Serializer
+{
+    let peer_id_bytes = peer_id.to_bytes();
+    serializer.serialize_bytes(&peer_id_bytes)
+}
+
+fn deserialize_peer_id<'de, D>(deserializer: D) -> Result<PeerId, D::Error>
+where 
+    D: Deserializer<'de>
+{
+    let peer_id_bytes = <Vec<u8>>::deserialize(deserializer)?;
+    PeerId::from_bytes(&peer_id_bytes)
+        .map_err(|e| serde::de::Error::custom(format!("could not deserialize PeerId: {}", e)))
 }
