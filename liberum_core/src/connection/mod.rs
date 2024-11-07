91ce5b38d5ac48d185f9cbb12bec95979177d5df
Maciej Krzyżanowski <krzmaciek@gmail.com>
2024-11-08 00:09:27 +0100

    core::connection: Refactor

--- liberum_core/src/connection/mod.rs
+++ liberum_core/src/connection/mod.rs
@@ -1,42 +1,33 @@
-mod message_handling;
+use std::path::PathBuf;
+
+use crate::node;
+use crate::node::manager::GetNode;
 use crate::node::manager::NodeManager;
 use crate::node::store::NodeStore;
+use crate::node::DownloadFile;
+use crate::node::GetProviders;
+use crate::node::Node;
+use crate::node::PublishFile;
 use anyhow::Result;
 use futures::prelude::*;
 use kameo::actor::ActorRef;
+use kameo::request::MessageSend;
 use liberum_core::codec::AsymmetricMessageCodec;
 use liberum_core::messages::*;
-use message_handling::*;
+use libp2p::identity::Keypair;
 use tokio::net::UnixListener;
 use tokio_util::codec::Decoder;
 use tokio_util::codec::Framed;
 use tracing::{debug, info, warn};
 
-/// Used by the core daemon to listen for incoming connections from UI
-/// Only one UI connection is possible at a time
-
-async fn handle_message(message: DaemonRequest, context: &AppContext) -> DaemonResult {
-    match message {
-        DaemonRequest::NewNode { name } => handle_new_node(name, context).await,
-        DaemonRequest::StartNode { name } => handle_start_node(name, context).await,
-        DaemonRequest::StopNode { name } => handle_stop_nodes(name, context).await,
-        DaemonRequest::ListNodes => handle_list_nodes(context).await,
-        DaemonRequest::PublishFile { node_name, path } => {
-            handle_publish_file(&node_name, path, context).await
-        }
-        DaemonRequest::DownloadFile { node_name, id } => {
-            handle_download_file(node_name, id, context).await
-        }
-        DaemonRequest::GetProviders { node_name, id } => {
-            handle_get_providers(node_name, id, context).await
-        }
-    }
-}
+type SocketFramed =
+    Framed<tokio::net::UnixStream, AsymmetricMessageCodec<DaemonResult, DaemonRequest>>;
 
 #[derive(Clone)]
 struct AppContext {
     node_manager: ActorRef<NodeManager>,
 }
+
 impl AppContext {
     fn new(node_store: ActorRef<NodeStore>) -> Self {
         AppContext {
@@ -45,11 +36,26 @@ impl AppContext {
     }
 }
 
+pub async fn listen(listener: UnixListener) -> Result<()> {
+    info!("Server listening on {:?}", listener);
+    let mut id = 0;
+    let app_context = AppContext::new(kameo::spawn(NodeStore::with_default_nodes_dir().await?));
+    loop {
+        let (daemon_socket, _) = listener.accept().await?;
+        info!(conn_id = id, "Handling a new connection");
+        let daemon_socket_framed: SocketFramed =
+            AsymmetricMessageCodec::new().framed(daemon_socket);
+        tokio::spawn(handle_connection(
+            daemon_socket_framed,
+            id.clone(),
+            app_context.clone(),
+        ));
+        id = id.wrapping_add(1);
+    }
+}
+
 async fn handle_connection(
-    mut daemon_socket_framed: Framed<
-        tokio::net::UnixStream,
-        AsymmetricMessageCodec<DaemonResult, DaemonRequest>,
-    >,
+    mut daemon_socket_framed: SocketFramed,
     id: u64,
     app_context: AppContext,
 ) -> Result<()> {
@@ -74,22 +80,137 @@ async fn handle_connection(
     Ok(())
 }
 
-pub async fn listen(listener: UnixListener) -> Result<()> {
-    info!("Server listening on {:?}", listener);
-    let mut id = 0;
-    let app_context = AppContext::new(kameo::spawn(NodeStore::with_default_nodes_dir().await?));
-    loop {
-        let (daemon_socket, _) = listener.accept().await?;
-        debug!(conn_id = id, "Handling a new connection");
-        let daemon_socket_framed: Framed<
-            tokio::net::UnixStream,
-            AsymmetricMessageCodec<DaemonResult, DaemonRequest>,
-        > = AsymmetricMessageCodec::new().framed(daemon_socket);
-        tokio::spawn(handle_connection(
-            daemon_socket_framed,
-            id.clone(),
-            app_context.clone(),
-        ));
-        id = id.wrapping_add(1);
+/// Used by the core daemon to listen for incoming connections from UI
+/// Only one UI connection is possible at a time
+async fn handle_message(message: DaemonRequest, context: &AppContext) -> DaemonResult {
+    match message {
+        DaemonRequest::NewNode { name: node_name } => handle_new_node(node_name, context).await,
+        DaemonRequest::StartNode { name: node_name } => handle_start_node(node_name, context).await,
+        DaemonRequest::StopNode { name: node_name } => handle_stop_nodes(node_name, context).await,
+        DaemonRequest::ListNodes => handle_list_nodes(context).await,
+        DaemonRequest::PublishFile { node_name, path } => {
+            handle_publish_file(&node_name, path, context).await
+        }
+        DaemonRequest::DownloadFile { node_name, id } => {
+            handle_download_file(node_name, id, context).await
+        }
+        DaemonRequest::GetProviders { node_name, id } => {
+            handle_get_providers(node_name, id, context).await
+        }
     }
+}
+
+async fn handle_new_node(name: String, context: &AppContext) -> DaemonResult {
+    let node = Node::builder()
+        .name(name)
+        .keypair(Keypair::generate_ed25519())
+        .build()
+        .map_err(|e| DaemonError::Other(e.to_string()))?;
+
+    let resp = context
+        .node_manager
+        .ask(node::manager::CreateNode { node })
+        .send()
+        .await;
+    match resp {
+        Err(e) => Err(DaemonError::Other(e.to_string())),
+        Ok(_resp) => Ok(DaemonResponse::NodeCreated),
+    }
+}
+
+async fn handle_start_node(name: String, context: &AppContext) -> DaemonResult {
+    context
+        .node_manager
+        .ask(node::manager::StartNode { name: name.clone() })
+        .send()
+        .await
+        .inspect_err(|e| debug!(err = e.to_string(), "Failed to handle start node"))
+        .map_err(|e| DaemonError::Other(e.to_string()))?;
+
+    debug!(name = name, "Node started!");
+
+    Ok(DaemonResponse::NodeStarted)
+}
+
+async fn handle_stop_nodes(name: String, context: &AppContext) -> DaemonResult {
+    let resp = context
+        .node_manager
+        .ask(node::manager::StopNode { name })
+        .send()
+        .await
+        .map_err(|e| DaemonError::Other(e.to_string()));
+    match resp {
+        Err(e) => Err(DaemonError::Other(e.to_string())),
+        Ok(_nodes) => Ok(DaemonResponse::NodeStopped),
+    }
+}
+
+async fn handle_list_nodes(_context: &AppContext) -> DaemonResult {
+    Ok(DaemonResponse::NodeList(vec![]))
+}
+
+async fn handle_publish_file(node_name: &str, path: PathBuf, context: &AppContext) -> DaemonResult {
+    let node = context
+        .node_manager
+        .ask(GetNode {
+            name: node_name.to_string(),
+        })
+        .send()
+        .await
+        .inspect_err(|e| debug!(err = e.to_string(), "Failed to handle publish file"))
+        .map_err(|e| DaemonError::Other(e.to_string()))?;
+
+    let resp_id = node
+        .ask(PublishFile { path })
+        .send()
+        .await
+        .inspect_err(|e| debug!(err = e.to_string(), "Failed to handle publish file"))
+        .map_err(|e| DaemonError::Other(e.to_string()))?;
+
+    Ok(DaemonResponse::FilePublished { id: resp_id })
+}
+
+async fn handle_get_providers(node_name: String, id: String, context: &AppContext) -> DaemonResult {
+    let node = context
+        .node_manager
+        .ask(GetNode {
+            name: node_name.clone(),
+        })
+        .send()
+        .await
+        .inspect_err(|e| debug!(err = e.to_string(), "Failed to get file providers"))
+        .map_err(|e| DaemonError::Other(e.to_string()))?;
+
+    let resp = node
+        .ask(GetProviders { id: id.clone() })
+        .send()
+        .await
+        .inspect_err(|e| debug!(err = e.to_string(), "Failed to get file providers"))
+        .map_err(|e| DaemonError::Other(e.to_string()))?;
+
+    Ok(DaemonResponse::Providers {
+        ids: resp.iter().map(|r| r.to_base58()).collect(),
+    })
+}
+
+// TODO! Downloading a file is blocking now, it should be done in background in some way
+async fn handle_download_file(node_name: String, id: String, context: &AppContext) -> DaemonResult {
+    let node = context
+        .node_manager
+        .ask(GetNode {
+            name: node_name.to_string(),
+        })
+        .send()
+        .await
+        .inspect_err(|e| debug!(err = e.to_string(), "Failed to handle download file"))
+        .map_err(|e| DaemonError::Other(e.to_string()))?;
+
+    let file = node
+        .ask(DownloadFile { id })
+        .send()
+        .await
+        .inspect_err(|e| debug!(err = e.to_string(), "Failed to handle download file"))
+        .map_err(|e| DaemonError::Other(e.to_string()))?;
+
+    Ok(DaemonResponse::FileDownloaded { data: file })
 }
