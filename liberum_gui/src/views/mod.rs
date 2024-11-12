32f09f87e5bdd58d335cf5d7cd48f09e8ef94c7d
Maciej Krzyżanowski <krzmaciek@gmail.com>
2024-11-13 00:37:35 +0100

    gui: Add observing configs to SystemObserver

--- liberum_gui/src/views/mod.rs
+++ liberum_gui/src/views/mod.rs
@@ -4,10 +4,13 @@ pub mod nodes_list_view;
 pub use node_view::NodeView;
 pub use nodes_list_view::NodesListView;
 
+use std::cell::RefCell;
+use std::rc::Rc;
 use std::sync::Arc;
 
 use std::sync::Mutex;
 
+use crate::system_observer::SystemObserver;
 use crate::{event_handler::EventHandler, system_observer::SystemState};
 
 pub trait AppView {
@@ -21,6 +24,7 @@ pub enum ViewAction {
 
 pub struct ViewContext<'a> {
     pub system_state: Arc<Mutex<Option<SystemState>>>,
+    pub system_observer: Rc<RefCell<SystemObserver>>,
     pub event_handler: &'a mut EventHandler,
     pub egui_ctx: &'a egui::Context,
     pub _egui_frame: &'a mut eframe::Frame,
