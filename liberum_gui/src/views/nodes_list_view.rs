105995dad1da049575c367e2dd9c6b6e6694e81c
Maciej Krzyżanowski <krzmaciek@gmail.com>
2024-11-13 01:44:09 +0100

    gui: Improve NodeListView

--- liberum_gui/src/views/nodes_list_view.rs
+++ liberum_gui/src/views/nodes_list_view.rs
@@ -22,48 +22,58 @@ impl AppView for NodesListView {
                 }
             };
 
-            ui.heading("Create a new node");
-            ui.horizontal(|ui| {
-                ui.text_edit_singleline(&mut self.create_node_name);
+            ui.heading("Nodes list page");
 
-                if ui.button("Create").clicked() {
-                    ctx.event_handler
-                        .create_node(&mut self.create_node_name)
-                        .unwrap();
+            egui::Window::new("Nodes")
+                .default_pos([32.0, 64.0])
+                .show(ctx.egui_ctx, |ui| {
+                    ui.label("Create a new node");
+                    ui.horizontal(|ui| {
+                        ui.text_edit_singleline(&mut self.create_node_name);
 
-                    self.create_node_name = String::new();
-                }
-            });
+                        if ui.button("Create").clicked() {
+                            ctx.event_handler
+                                .create_node(&mut self.create_node_name)
+                                .unwrap();
 
-            ui.add_space(10.0);
-
-            ui.heading("Nodes list:");
-            state.node_infos.iter().for_each(|n| {
-                ui.horizontal(|ui| {
-                    ui.vertical(|ui| {
-                        ui.colored_label(
-                            Color32::from_rgb(0, 100, 200),
-                            format!("Node: {}", n.name),
-                        );
-                        ui.label(format!("Is running: {}", n.is_running));
-                        ui.add_space(10.0);
+                            self.create_node_name = String::new();
+                        }
                     });
 
-                    if ui.button("Run").clicked() {
-                        let _ = ctx.event_handler.run_node(&n.name);
-                    }
+                    ui.add_space(20.0);
 
-                    if ui.button("Stop").clicked() {
-                        let _ = ctx.event_handler.stop_node(&n.name);
-                    }
+                    egui::Grid::new("config_grid")
+                        .num_columns(3)
+                        .striped(true)
+                        .show(ui, |ui| {
+                            state.node_infos.iter().for_each(|n| {
+                                ui.colored_label(
+                                    Color32::from_rgb(0, 100, 200),
+                                    format!("Node: {}", n.name),
+                                );
 
-                    if ui.button("Show").clicked() {
-                        action = ViewAction::SwitchView {
-                            view: Box::new(NodeView::new(&n.name)),
-                        };
-                    }
+                                ui.label(format!("Is running: {}", n.is_running));
+
+                                ui.horizontal(|ui| {
+                                    if ui.button("Run").clicked() {
+                                        let _ = ctx.event_handler.run_node(&n.name);
+                                    }
+
+                                    if ui.button("Stop").clicked() {
+                                        let _ = ctx.event_handler.stop_node(&n.name);
+                                    }
+
+                                    if ui.button("Show").clicked() {
+                                        action = ViewAction::SwitchView {
+                                            view: Box::new(NodeView::new(&n.name)),
+                                        };
+                                    }
+                                });
+
+                                ui.end_row();
+                            });
+                        });
                 });
-            });
 
             ui.add_space(10.0);
         });
