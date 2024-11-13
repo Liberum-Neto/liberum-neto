2a7b37bb266894b7bb66966ed47f21b3ca420ce2
Maciej Krzyżanowski <krzmaciek@gmail.com>
2024-11-13 02:58:59 +0100

    gui: Add dialer

--- liberum_gui/src/event_handler.rs
+++ liberum_gui/src/event_handler.rs
@@ -158,7 +158,7 @@ impl EventHandler {
                     match r {
                         Ok(DaemonResponse::FileDownloaded { data }) => return Ok(data),
                         Err(e) => {
-                            error!(err = e.to_string(), "Error ocurred while publishing file!");
+                            error!(err = e.to_string(), "Error ocurred while downloading file!");
                             bail!("Error occured while publishing file: {}", e.to_string());
                         }
                         _ => {
@@ -172,6 +172,40 @@ impl EventHandler {
                     bail!("Failed to receive response from the daemon");
                 }
             }
+        })
+    }
+
+    pub fn dial(&mut self, node_name: &str, peer_id: &str, addr: &str) -> Result<()> {
+        self.rt.block_on(async {
+            self.to_daemon_sender
+                .send(DaemonRequest::Dial {
+                    node_name: node_name.to_string(),
+                    peer_id: peer_id.to_string(),
+                    addr: addr.to_string(),
+                })
+                .await?;
+
+            match self.from_daemon_receiver.recv().await {
+                Some(r) => {
+                    match r {
+                        Ok(DaemonResponse::Dialed) => {}
+                        Err(e) => {
+                            error!(err = e.to_string(), "Error ocurred while dialing peer!");
+                            bail!("Error occured while publishing file: {}", e.to_string());
+                        }
+                        _ => {
+                            error!("Unexpected response type");
+                            bail!("Unexpected response type");
+                        }
+                    };
+                }
+                None => {
+                    error!("Failed to receive response");
+                    bail!("Failed to receive response from the daemon");
+                }
+            };
+
+            Ok(())
         })
     }
 }
