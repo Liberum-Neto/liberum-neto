422067397eb85f4f880680e0ec69215d8d00c37e
Filip Gołaś <philipgoclever@gmail.com>
2024-12-08 14:30:13 +0100

    incremental-test.py: Test that draws plots
    Signed-off-by: Filip Gołaś <philipgoclever@gmail.com>

--- incremental-test.py
+++ incremental-test.py
@@ -4,9 +4,10 @@ import numpy as np
 import os
 import subprocess
 
-CORE_BIN = "./target/debug/liberum_core"
-CLI_BIN = "./target/debug/liberum_cli"
-NODE_COUNT = 20
+CORE_BIN = "./target/release/liberum_core"
+CLI_BIN = "./target/release/liberum_cli"
+INIT_COUNT = 5
+NODE_COUNT = 25
 FILE_NAME = os.path.dirname(os.path.realpath(__file__)) + "/test-file.txt"
 FILE_CONTENT = "Hello, World!"
 
@@ -41,6 +42,7 @@ with open(FILE_NAME, mode="w") as f:
 
 
 RESULTS=[]
+FIND_TIMES=[]
 for i in range (1, NODE_COUNT) :
     N = "test_n" + str(i)
     N_ADDR = NODE_ADDR_PREFIX + str(i + 52136) + NODE_ADDR_SUFFIX
@@ -48,7 +50,7 @@ for i in range (1, NODE_COUNT) :
     subprocess.run([CLI_BIN, "-d", "new-node", N, "--id-seed", str(i)])
     subprocess.run([CLI_BIN, "-d", "config-node", N, "add-external-addr", N_ADDR])
     if i > 0:
-        subprocess.run([CLI_BIN, "config-node", N, "add-bootstrap-node", N_IDS[i -1], N_ADDRESSES[i-1]])
+        subprocess.run([CLI_BIN, "config-node", N, "add-bootstrap-node", N_IDS[i-1], N_ADDRESSES[i-1]])
 
     subprocess.run([CLI_BIN, "-d", "start-node", N])
 
@@ -56,27 +58,49 @@ for i in range (1, NODE_COUNT) :
     N_NAMES.append(N)
     N_IDS.append(ID)
     N_ADDRESSES.append(N_ADDR)
-    time.sleep(0.2)
-    if i == 3:
+    time.sleep(0.01)
+    if i == INIT_COUNT:
         FILE_ID=subprocess.run([CLI_BIN, "publish-file", N_NAMES[0], FILE_NAME], stdout=subprocess.PIPE).stdout.decode().strip()
-    if i > 3:
+    if i > INIT_COUNT:
         RESULTS.append([])
+        FIND_TIMES.append(0)
 
-        for j in range(0, i):
-            RESULT=subprocess.run([CLI_BIN, "-d", "download-file", N_NAMES[i], FILE_ID], stdout=subprocess.PIPE).stdout.decode().strip()
+        for j in range(1, i):
+            t0 = time.time()
+            RESULT=subprocess.run([CLI_BIN, "-d", "download-file", N_NAMES[j], FILE_ID], stdout=subprocess.PIPE).stdout.decode().strip()
+            t = time.time()-t0
             cmp = FILE_CONTENT == RESULT
-            print(FILE_CONTENT, RESULT)
-            print(cmp)
-            RESULTS[i-4].append(cmp)
+            RESULTS[-1].append(cmp)
+            FIND_TIMES[-1] += t
+        FIND_TIMES[-1] = (FIND_TIMES[-1] / i) * 1000.0
 
 for x in range(0, len(RESULTS)):
     data = [i for i in range(len(RESULTS[x])) if RESULTS[x][i] == True]
     X = [x] * len(data)
     plt.scatter(X, data)
+plt.title("Udane wyszukania, " + str(INIT_COUNT) + " węzłów w sieci w chwili publikacji")
+plt.ylabel("Numer węzła")
+plt.xlabel("Ilość nowych węzłów dodanych do sieci")
+plt.show()
+
+failed_counts = []
+for x in range(0, len(RESULTS)):
+    data = [i for i in range(len(RESULTS[x])) if RESULTS[x][i] == True]
+    failed = len(data) - len(RESULTS[x])
+    failed_counts.append(failed)
+plt.plot(failed_counts)
+plt.title("Liczba nieudanch wyszukań, " + str(INIT_COUNT) + " węzłów w sieci w chwili publikacji")
+plt.ylabel("iczba nieudanch wyszukań")
+plt.xlabel("Ilość nowych węzłów dodanych do sieci")
+plt.show()
+
+plt.plot(FIND_TIMES)
+plt.title("Średni czas odnajdywania, " + str(INIT_COUNT) + " węzłów w sieci w chwili publikacji")
+plt.ylabel("Czas [ms]")
+plt.xlabel("Ilość nowych węzłów dodanych do sieci")
 plt.show()
 
 for i in range(0, NODE_COUNT):
     subprocess.run([CLI_BIN, "stop-node", N_NAMES[i]])
 
 subprocess.run(["killall", "liberum_core"])
-print(RESULTS)
