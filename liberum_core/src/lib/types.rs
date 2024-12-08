0fcb3d9704dd34e49f8451cb8f0563b706c4fcdd
Maciej Krzyżanowski <krzmaciek@gmail.com>
2024-12-08 20:31:50 +0100

    [#59] Print node run addresses

--- liberum_core/src/lib/types.rs
+++ liberum_core/src/lib/types.rs
@@ -15,9 +15,3 @@ pub struct TypedObjectInfo {
     pub id: String,
     pub type_id: Uuid,
 }
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct TypedObjectInfo {
-    pub id: String,
-    pub type_id: Uuid,
-}
