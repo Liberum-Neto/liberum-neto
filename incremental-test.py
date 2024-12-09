e8d88920727a6949d6e6d526f8b1ee8b86d74d7c
Filip Gołaś <philipgoclever@gmail.com>
2024-12-09 16:28:22 +0100

    incremental-test.py: Save measurements to files
    Signed-off-by: Filip Gołaś <philipgoclever@gmail.com>

--- incremental-test.py
+++ incremental-test.py
@@ -4,14 +4,29 @@ import numpy as np
 import os
 import subprocess
 import random
+import socketserver
+import pickle
+
+
+with open("sukcesy.pkl", "rb") as f:
+    RESULTS = pickle.load(f)
+with open("czasy.pkl", "rb") as f:
+    FIND_TIMES = pickle.load(f)
+FIND_TIMES.sort()
+print(FIND_TIMES[-10:])
+exit(0)
+
+def find_free_port():
+    with socketserver.TCPServer(("localhost", 0), None) as s:
+        return str(s.server_address[1])
 
 CORE_BIN = "./target/release/liberum_core"
 CLI_BIN = "./target/release/liberum_cli"
-INIT_COUNT = 5
-NODE_COUNT = 50
-MEASURE_EVERY=3
-DOWNLOAD_EVERY=3
-DOWNLOAD_PERCENT=50
+INIT_COUNT = 20
+NODE_COUNT = 1000
+MEASURE_EVERY=10
+DOWNLOAD_EVERY=1
+DOWNLOAD_PERCENT=100
 FILE_NAME = os.path.dirname(os.path.realpath(__file__)) + "/test-file.txt"
 FILE_CONTENT = "Hello, World!"
 AVG_CLI_TIME_NORMALIZATION=1.6 # ms
@@ -35,6 +50,12 @@ subprocess.run([CLI_BIN, "-d", "new-node", N, "--id-seed", "0"])
 subprocess.run([CLI_BIN, "-d", "config-node", N, "add-external-addr", N_ADDR])
 
 subprocess.run([CLI_BIN, "-d", "start-node", N])
+N_ADDR = NODE_ADDR_PREFIX + find_free_port() + NODE_ADDR_SUFFIX
+
+subprocess.run([CLI_BIN, "new-node", N, "--id-seed", "0"])
+subprocess.run([CLI_BIN, "config-node", N, "add-external-addr", N_ADDR])
+
+subprocess.run([CLI_BIN, "start-node", N])
 
 ID = subprocess.run([CLI_BIN, "-d", "get-peer-id", N], stdout=subprocess.PIPE).stdout.decode().strip()
 N_NAMES.append(N)
@@ -72,6 +93,28 @@ for i in range (1, NODE_COUNT) :
     N_IDS.append(ID)
     N_ADDRESSES.append(N_ADDR)
     time.sleep(0.01)
+for i in range (1, NODE_COUNT+1) :
+    N = "test_n" + str(i)
+
+    while True:
+        subprocess.run([CLI_BIN, "new-node", N, "--id-seed", str(i)])
+        subprocess.run([CLI_BIN, "config-node", N, "add-bootstrap-node", N_IDS[i-1], N_ADDRESSES[i-1]])
+
+        N_ADDR = NODE_ADDR_PREFIX + find_free_port() + NODE_ADDR_SUFFIX
+        print(N, N_ADDR)
+        subprocess.run([CLI_BIN, "config-node", N, "add-external-addr", N_ADDR])
+        subprocess.run([CLI_BIN, "start-node", N])
+
+        id_cmd = subprocess.run([CLI_BIN, "-d", "get-peer-id", N], stdout=subprocess.PIPE)
+        if id_cmd.returncode == 0 :
+            break
+        time.sleep(0.1)
+
+    ID = id_cmd.stdout.decode().strip()
+    N_NAMES.append(N)
+    N_IDS.append(ID)
+    N_ADDRESSES.append(N_ADDR)
+    time.sleep(0.02)
     if i == INIT_COUNT:
         FILE_ID=subprocess.run([CLI_BIN, "publish-file", N_NAMES[0], FILE_NAME], stdout=subprocess.PIPE).stdout.decode().strip()
     if i > INIT_COUNT:
@@ -81,6 +124,7 @@ for i in range (1, NODE_COUNT) :
             measurements=0
             for j in range(1, i):
                 if j % DOWNLOAD_EVERY == 0 and random.random()*100 > DOWNLOAD_PERCENT:
+                if j % DOWNLOAD_EVERY == 0 and random.random()*100 <= DOWNLOAD_PERCENT:
                     measurements+=1
                     t0 = time.time()
                     RESULT=subprocess.run([CLI_BIN, "-d", "download-file", N_NAMES[j], FILE_ID], stdout=subprocess.PIPE).stdout.decode().strip()
@@ -90,6 +134,12 @@ for i in range (1, NODE_COUNT) :
                     find_tiems_temp[1] += t
                 if find_tiems_temp[1] > AVG_CLI_TIME_NORMALIZATION:
                     find_tiems_temp[1] = (find_tiems_temp[1] / measurements) * 1000.0 - AVG_CLI_TIME_NORMALIZATION
+                    if not cmp:
+                        print(FILE_CONTENT, RESULT)
+                    results_temp[1].append((j,cmp))
+                    find_tiems_temp[1] += t
+            if find_tiems_temp[1] > 0:
+                find_tiems_temp[1] = (find_tiems_temp[1] / measurements) * 1000.0 - AVG_CLI_TIME_NORMALIZATION
             if measurements > 0:
                 RESULTS.append(results_temp)
                 FIND_TIMES.append(find_tiems_temp)
@@ -130,6 +180,7 @@ plt.show()
 X = [x[0] for x in FIND_TIMES]
 Y = [x[1] for x in FIND_TIMES]
 plt.plot(X, Y, 'b')
+plt.yscale('log')
 plt.title("Średni czas odnajdywania, B=" + str(INIT_COUNT) + ", N=" +  str(NODE_COUNT))
 plt.ylabel("Czas [ms]")
 plt.xlabel("n - ilość węzłów w sieci")
@@ -141,3 +192,8 @@ for i in range(0, NODE_COUNT):
     subprocess.run([CLI_BIN, "stop-node", N_NAMES[i]])
 
 subprocess.run(["killall", "liberum_core"])
+
+with open("sukcesy.pkl", "wb") as f:
+    pickle.dump(RESULTS, f)
+with open("czasy.pkl", "wb") as f:
+    pickle.dump(FIND_TIMES, f)
