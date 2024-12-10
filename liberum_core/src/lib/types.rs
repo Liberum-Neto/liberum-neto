c10e96c008281c8dd41ea61ccbce88d8b6d45257
Maciej Krzyżanowski <krzmaciek@gmail.com>
2024-12-10 17:38:26 +0100

    [#70] Return and print PeerId of nodes

--- liberum_core/src/lib/types.rs
+++ liberum_core/src/lib/types.rs
@@ -4,6 +4,7 @@ use uuid::Uuid;
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct NodeInfo {
     pub name: String,
+    pub peer_id: String,
     pub is_running: bool,
     pub config_addresses: Vec<String>,
     pub running_addresses: Vec<String>,
