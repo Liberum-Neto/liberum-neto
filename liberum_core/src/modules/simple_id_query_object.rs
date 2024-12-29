f21d64ecf8efed91d6510734393cc0c8e0541133
lukgla <52761889+lukgla@users.noreply.github.com>
2024-12-29 17:21:20 +0100

    simple_id_query

---
+++ liberum_core/src/modules/simple_id_query_object.rs
@@ -0,0 +1,42 @@
+use async_trait::async_trait;
+use liberum_core::{
+    module::{Module, ModuleQueryParams, ModuleStoreParams},
+    parser::{parse_typed, ObjectEnum},
+    proto::{Hash, SignedObject, SimpleIDQuery, TypedObject},
+};
+use uuid::Uuid;
+
+pub struct SimpleIDQueryModule {}
+
+#[async_trait]
+impl Module for SignedObjectModule {
+    async fn publish(&self, object: TypedObject) -> (Option<TypedObject>, Option<Vec<Hash>>) {
+        return (None, None);
+    }
+
+    async fn store(&self, params: ModuleStoreParams) -> ModuleStoreParams {
+        ModuleStoreParams {
+            object: None,
+            signed_objects_hashes: params.signed_objects_hashes,
+        }
+    }
+
+    async fn query(&self, params: ModuleQueryParams) -> ModuleQueryParams {
+        if let ObjectEnum::SimpleIDQuery(obj) = parse_typed(params.object.unwrap()).await.unwrap() {
+            ModuleQueryParams {
+                matched_object_id: Some(vec![obj.id]),
+                object: None,
+            }
+        } else {
+            ModuleQueryParams {
+                matched_object_id: params.matched_object_id,
+                object: None,
+            }
+        }
+    }
+
+    fn register_module(&self) -> Vec<Uuid> {
+        return vec![SimpleIDQuery::UUID];
+    }
+
+}
